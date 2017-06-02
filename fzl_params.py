f5876826f049f595d38638f7c24eb5132d157c12
TruckThunders <nigelmathes@gmail.com>
2017-06-02 09:00:48 -0700

    Update params

--- fzl_params.py
+++ fzl_params.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 # Path to atomic data file
-transition_library = np.genfromtxt('/home/achernar-data/mathes/VULTURE/UVES_SQUAD/atoms_UVES.dat',unpack=True,dtype=None)
+transition_library = '/home/achernar-data/mathes/VULTURE/UVES_SQUAD/atoms_UVES.dat'
 
 # Ionic transitions to find
 search_ions = ['CIV', 'MgII']
